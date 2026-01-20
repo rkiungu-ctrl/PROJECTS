1181f599fb31d447d9bccae15c83c4da9723bf06
Rachel Kiungu <r.kiungu@tandaa.africa>
2026-01-20 20:20:36 +0300

    Add PurchaseInvoice and PurchaseInvoiceLine to models/__init__.py for procurement discoverability

--- AccountingSystem/models/__init__.py
+++ AccountingSystem/models/__init__.py
@@ -1,27 +1,29 @@
-from .employee import Employee
-from .payroll import Payroll
-from .account import Account, AccountSettings
-from .journal import JournalEntry, JournalLine
-from .customer import Customer
-from .product import Product
-from .stock_entry import StockEntry
-from .payslip import Payslip
-from .receipt import PaymentReceipt
-from .company import CompanyProfile
-from .supplier import Supplier
- 
-from .bank_rule import BankRule
-from .bank_payment_line import BankPaymentLine
+from .employee.employee import Employee
+from .employee.claims import EmployeeClaim, EmployeeClaimLine
+from .payroll.payroll import Payroll
+from .settings.account import Account, AccountSettings
+from .accounting.journal import JournalEntry, JournalLine
+from .sales.customer import Partner
+from .procurement.product import Product
+from .procurement.stock_entry import StockEntry
+from .payroll.payslip import Payslip
+from .bank.receipt import PaymentReceipt
+from .settings.company import CompanyProfile
+from .procurement.supplier import Supplier
 
-from .purchase_invoice import PurchaseInvoice, PurchaseInvoiceLine
-from .invoice import Invoice
-from .invoice_line import InvoiceLine
-from .tax import Tax
-from .increment import Increment
-from .shif_setting import SHIFSetting
-from .loan_advance import LoanAdvance
-from .loan_repayment import LoanRepayment
-from .non_cash_benefit import EmployeeNonCashBenefit
-from .nssf_setting import NSSFSetting
-from .bank_account_profile import BankAccountProfile
-from .currency import Currency
+from .bank import BankRule, BankTransactionV2, BankPaymentLine
+
+from .sales.invoice import Invoice
+from .sales.invoice_line import InvoiceLine
+from .procurement.purchase import PurchaseInvoice
+from .procurement.purchase_line import PurchaseInvoiceLine
+from .settings.tax import Tax
+from .employee.increment import Increment
+from .payroll.shif_setting import SHIFSetting
+from .employee.loans import LoanAdvance
+from .payroll.loan_repayment import LoanRepayment
+from .payroll.non_cash_benefit import EmployeeNonCashBenefit
+from .payroll.nssf_setting import NSSFSetting
+from .bank import BankAccountProfile
+from .settings.currency import Currency
+from .payroll.payroll_posting_rule import PayrollPostingRule
